e50af68bc9c81f6e49a2195ad466842bc9a291ee
MARTIN ZAPATA <zapatafergusonm@gmail.com>
2025-05-14 03:55:20 +0200

    Made all graphs, missing statistics

--- spin_chain.f90
+++ spin_chain.f90
@@ -128,7 +128,7 @@ program orth_polys
         if (ios /= 0) stop "Error opening spin_chain_data/eigenvectors.dat file"
 
         ! Write three arrays as rows
-        write(20, '(*(G0.6,:," "))') (- E(i), i = 1, N)
+        write(20, '(*(G0.6,:," "))') (- E(i), i = N, 1, -1)
         write(20, '(*(G0.6,:," "))') (w(i), i = 1, N)
         write(20, '(*(G0.6,:," "))') (exp(log_g(i)), i = 1, N)
 
